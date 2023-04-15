f03ee404af6dddba68eb445710f0536153fbd4a5
Courage <couragesblog@gmail.com>
2023-04-15 14:36:13 +0100

    Create Home view

---
+++ src/views/HomePage.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { storeToRefs } from 'pinia';
+import { useMovieStore } from '../stores/movie.js';
+import MovieList from '../components/MovieList.vue';
+
+const { filteredMovies } = storeToRefs(useMovieStore());
+const movieStore = useMovieStore();
+
+movieStore.getMovies();
+
+
+</script>
+
+<template>
+  <MovieList :movies="filteredMovies" />
+</template>
