80a480901a154f8e8678648cde797eefe51daac3
Courage <couragesblog@gmail.com>
2023-04-15 12:08:50 +0100

    Scaffold app layout

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <div class="flex flex-col bg-blue-100 h-screen overflow-y-auto">
+    <div class="w-[1100px] mx-auto my-2">
+      <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+        <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+          <div class="bg-white divide-y divide-gray-200">
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
